9911bb5b7feb3c1bee57bbc259e112d3b3aeb83e
sebamomann <github@sebamomann.de>
2024-01-13 14:30:35 +0100

    offline service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,57 +1,101 @@
 // service-worker.js
 const CACHE_NAME = "version-1";
-const urlsToCache = ['index.html', 'offline.html'];
+const urlsToCache = [
+  'index.html',
+  '/css/main.css', // Add your CSS files
+  '/js/app.js', // Add your JavaScript files
+  '/images/logo.png', // Add essential images
+  '/**/*'
+];
 
-const self = this;
+let cacheVersion = 0;
+let cacheName = `cache-v${cacheVersion}`;
 
-// Install SW
-self.addEventListener('install', (event) => {
+function increment() {
+  cacheVersion++;
+  cacheName = `cache-v${cacheVersion}`;
+}
+
+self.addEventListener("install", (event) => {
+  console.log("Attempting to install service worker and cache static assets");
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches
+      .open(cacheName)
       .then((cache) => {
-        console.log('Opened cache');
+        //Update version
+        increment();
 
-        return cache.addAll(urlsToCache);
+        //add files to the cache
+        return cache.addAll(staticAssets);
       })
+      .catch((err) => console.log(err))
   );
 });
 
 // Listen for requests
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
+
   event.respondWith(
-    caches.match(event.request)
-      .then(() => {
-        return fetch(event.request)
-          .catch(() => caches.match('offline.html'));
+    caches
+      .match(event.request)
+      .then((response) => {
+        //If the response is found in the cache
+        if (response) {
+          console.log("Found ", event.request.url, " in cache");
+          return response;
+        }
+
+        return fetch(event.request).then((response) => {
+          // If a response is not found
+          if (response.status === 404) {
+            return caches.open(cacheName).then((cache) => {
+              return cache.match("404.html");
+            });
+          }
+
+          //Caching and returning the response if it doesn't exist in the cache
+          return caches.open(cacheName).then((cache) => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+      })
+      .catch(async (error) => {
+        console.log("Error, ", error);
+        //If page is offline/ Network failure
+        return caches.open(cacheName).then((cache) => {
+          return cache.match("offline.html");
+        });
       })
   );
 });
 
-// Activate the SW
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [];
-  cacheWhitelist.push(CACHE_NAME);
+self.addEventListener("activate", (event) => {
+  console.log("Activating new service worker...");
 
   event.waitUntil(
-    caches.keys().then((cacheNames) => Promise.all(
-      cacheNames.map((cacheName) => {
-        if (!cacheWhitelist.includes(cacheName)) {
-          return caches.delete(cacheName);
-        }
-      })
-    ))
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((storedCacheName) => {
+          if (storedCacheName !== cacheName) {
+            return caches.delete(storedCacheName);
+          }
+        })
+      );
+    })
   );
 });
 
 
+// Push notifications handler
 self.addEventListener('push', event => {
   const data = event.data.json().notification;
 
-  const title = data.title || JSON.stringify(data);
+  const title = data.title || 'Notification';
   const options = {
     body: data.body || 'You have a new message.',
-    icon: data.icon || '/path/to/default-icon.png',
-    // Other options like 'badge', 'image', 'tag', etc.
+    icon: data.icon || '/images/default-icon.png',
+    // Other options
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
